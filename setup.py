9201cebe4645f21fb9a07770f36fe6baaee8cf03
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-01-26 09:28:36 -0500

    Clean up setup.py, add requirement

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
+"""Set up PyPI package."""
 from setuptools import setup, find_packages
 
-setup(name='fuzzer', version='1.0', packages=find_packages())
+setup(
+    name="fuzzer",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "exrex>=0.10,<0.11"
+    ]
+)
