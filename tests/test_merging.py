3ea2fcc0e4abac247ef9cbb117f4243e55e82212
Alon Greyber <alon@greyber.org>
2021-02-19 13:06:18 -0500

    Added a test to ensure merging doesn't modify parameters

--- tests/test_merging.py
+++ tests/test_merging.py
@@ -21,3 +21,14 @@ for filename in case_files:
 def test_merging(schemas, merged):
     """Test that merging the `schemas` results in the `merged` schema."""
     assert merge(*schemas) == merged
+
+
+def test_merge_doesnt_modify():
+    """ Test that merging doesn't modify input values """
+    required_a = ["required_property"]
+    a = {"required": required_a}
+    b = {"required": ["another_required_property"]}
+    c = merge(a, b)
+
+    assert len(c['required']) == 2
+    assert len(required_a) == 1
