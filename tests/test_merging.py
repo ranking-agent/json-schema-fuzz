e2489474eedc61c80ddfe23cc1871d0edfd8f7bc
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-02-19 09:53:56 -0500

    Add merge testing infrastructure

---
+++ tests/test_merging.py
@@ -0,0 +1,23 @@
+"""Test JSON schema merging."""
+import glob
+import json
+from pathlib import Path
+import pytest
+
+from json_schema_fuzz.merging import merge
+
+
+THIS_DIR = Path(__file__).parent
+CASE_DIR = THIS_DIR / "merge_cases"
+case_files = glob.glob(str(CASE_DIR / "*.json"))
+cases = []
+for filename in case_files:
+    with open(filename, "r") as stream:
+        case = json.load(stream)
+        cases.append((case["schemas"], case["merged"]))
+
+
+@pytest.mark.parametrize("schemas,merged", cases)
+def test_merging(schemas, merged):
+    """Test that merging the `schemas` results in the `merged` schema."""
+    assert merge(*schemas) == merged
