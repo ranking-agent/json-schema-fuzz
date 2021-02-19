65d4a8ee80b96bbe84b3d86b7d1718b39d6a6877
Alon Greyber <alon@greyber.org>
2021-02-19 13:52:24 -0500

    Restructured to reduce nesting

--- json_schema_fuzz/merging.py
+++ json_schema_fuzz/merging.py
@@ -19,17 +19,17 @@ def merge(
     # Make a copy of a so that it doesn't get modified in place
     a = copy.deepcopy(a)
     for key in b:
-        if key in a:
-            if isinstance(a[key], dict) and isinstance(b[key], dict):
-                merge(a[key], b[key], path + [str(key)])
-            elif a[key] == b[key]:
-                pass  # same leaf value
-            elif isinstance(a[key], list) and isinstance(b[key], list):
-                # Append lists together
-                a[key].extend(b[key])
-            else:
-                raise NotImplementedError(
-                    f"Conflicting key {key} encountered in path {path}")
-        else:
+        if key not in a:
             a[key] = b[key]
+            continue
+        if isinstance(a[key], dict) and isinstance(b[key], dict):
+            merge(a[key], b[key], path + [str(key)])
+        elif a[key] == b[key]:
+            pass  # same leaf value
+        elif isinstance(a[key], list) and isinstance(b[key], list):
+            # Append lists together
+            a[key].extend(b[key])
+        else:
+            raise NotImplementedError(
+                f"Conflicting key {key} encountered in path {path}")
     return a
