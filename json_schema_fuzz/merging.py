9cc1bf23991d5cb0362aedff9a3fc76e698c189f
Alon Greyber <alon@greyber.org>
2021-02-22 10:03:20 -0500

    Modified merge signature to specifically switch to in place merging for recursive call

--- json_schema_fuzz/merging.py
+++ json_schema_fuzz/merging.py
@@ -7,12 +7,13 @@ def merge(
     schema_a: Dict[Any, Any],
     schema_b: Dict[Any, Any],
     path: List[Any] = None,
+    in_place: bool = False,
 ):
     """Merge two JSON schemas recursively."""
     if path is None:
         path = []
 
-    if len(path) == 0:
+    if not in_place:
         # Make a copy of a so that it doesn't get modified in place
         schema_a = copy.deepcopy(schema_a)
 
@@ -21,7 +22,9 @@ def merge(
             schema_a[key] = schema_b[key]
             continue
         if isinstance(schema_a[key], dict) and isinstance(schema_b[key], dict):
-            merge(schema_a[key], schema_b[key], path + [str(key)])
+            merge(schema_a[key], schema_b[key],
+                  path=path + [str(key)],
+                  in_place=True)
         elif schema_a[key] == schema_b[key]:
             pass  # same leaf value
         elif (
@@ -33,4 +36,7 @@ def merge(
         else:
             raise NotImplementedError(
                 f"Conflicting key {key} encountered in path {path}")
-    return schema_a
+    if in_place:
+        return None
+    else:
+        return schema_a
