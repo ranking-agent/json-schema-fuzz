0819ac280bfaa9bb63a2496da3c901315c63a034
Alon Greyber <alon@greyber.org>
2021-04-05 10:20:33 -0400

    Added main file with click command

--- README.md
+++ README.md
@@ -9,6 +9,14 @@ Currently this tool is tied to *JSON Schema Draft 7*.
 Support for more recent JSON Schema versions are planned
 for the future.
 
+## Usage
+
+This tool can be imported as a module or run from the command line. To see usage information about the command line tool use the following command:
+
+```bash
+python -m json_schema_fuzz --help
+```
+
 ---
 
 This application is under development at **CoVar Applied Technologies Inc.**
---
+++ json_schema_fuzz/__main__.py
@@ -0,0 +1,30 @@
+import json
+
+import click
+
+from . import custom_json_loads, generate_json
+
+
+@click.command()
+@click.argument("schema-file", type=click.File("r"))
+@click.option("--count",
+              default=1,
+              help="Number of samples to generate")
+@click.option("--output-filename-prefix",
+              help="If given, write samples to files with the format \
+                    {prefix}{num}.json")
+def generate_json_command(schema_file, count, output_filename_prefix):
+    """ Generate JSON from schema using the command line """
+    schema = custom_json_loads(schema_file.read())
+    for index in range(count):
+        output_json = generate_json(schema)
+        if output_filename_prefix:
+            with open(f"{output_filename_prefix}{index}.json", "w+") as f:
+                json.dump(output_json, f, default=str)
+        else:
+            print(output_json)
+
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    generate_json_command()
