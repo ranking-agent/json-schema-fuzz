671560b694c380bcaed319a1257e2978159e569c
Alon Greyber <alon@greyber.org>
2021-04-05 11:24:55 -0400

    Fix linting issues

--- json_schema_fuzz/__main__.py
+++ json_schema_fuzz/__main__.py
@@ -1,3 +1,4 @@
+""" Command line interface """
 import json
 
 import click
@@ -19,8 +20,8 @@ def generate_json_command(schema_file, count, output_filename_prefix):
     for index in range(count):
         output_json = generate_json(schema)
         if output_filename_prefix:
-            with open(f"{output_filename_prefix}{index}.json", "w+") as f:
-                json.dump(output_json, f, default=str)
+            with open(f"{output_filename_prefix}{index}.json", "w+") as file:
+                json.dump(output_json, file, default=str)
         else:
             print(output_json)
 
